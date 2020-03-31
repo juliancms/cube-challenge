96b7aeef0d1ffb8be3f1e1738a243eb795efd362
Julián Marín <marin.julian@gmail.com>
2020-03-31 18:16:30 -0500

    Creation of main Cube Class

---
+++ models/Cube.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+class Cube
+{
+    private $m, $n, $cube;
+
+    function __construct($n, $m)
+    {
+        $this->m = $m;
+        $this->n = $n;
+        $this->buildCube($n);
+    }
+
+
+    private function buildCube($n)
+    {
+        for ($i = 0; $i <= $n; $i++) {
+            for ($j = 0; $j <= $n; $j++) {
+                for ($k = 0; $k <= $n; $k++) {
+                    $this->cube[$i][$j][$k] = 0;
+                }
+            }
+        }
+    }
+
+    public function updateValue($x, $y, $z, $value)
+    {
+        $this->cube[$x][$y][$z] = $value;
+    }
+
+    public function query($x1, $y1, $z1, $x2, $y2, $z2)
+    {
+        $sum = 0;
+        for ($i = $x1; $i <= $x2; $i++) {
+            for ($j = $y1; $j <= $y2; $j++) {
+                for ($k = $z1; $k <= $z2; $k++) {
+                    $sum += $this->cube[$i][$j][$k];
+                }
+            }
+        }
+        return $sum;
+    }
+
+    public function getM()
+    {
+        return $this->m;
+    }
+
+    public function getCube()
+    {
+        return $this->cube;
+    }
+
+    public function getN()
+    {
+        return $this->n;
+    }
+}
