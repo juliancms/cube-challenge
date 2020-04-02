a92d248c0e2028d5cc93a190e1893d001abdfc19
Julián Marín <marin.julian@gmail.com>
2020-04-02 17:10:35 -0500

    Update tests

--- tests/acceptance/HomeCest.php
+++ tests/acceptance/HomeCest.php
@@ -7,17 +7,15 @@ class HomeCest
     public function ensureThatHomePageWorks(AcceptanceTester $I)
     {
         $I->amOnPage(Url::toRoute('/site/index'));
-        $I->see('Cube Challenge');
+        $I->see('Number of Tests');
 
-        $I->amGoingTo('set the cube with integer values');
-        $I->fillField('input[name="cubeSetting-form[T_tests]"]', '2');
-        $I->fillField('input[name="cubeSetting-form[N_coordinates]"]', '4');
-        $I->fillField('input[name="cubeSetting-form[M_operations]"]', '5');
+        $I->amGoingTo('set the tests with an integer value');
+        $I->fillField('input[name="cubeTest-form[T_tests]"]', '2');
         $I->click('send-button');
         $I->wait(2); // wait for button to be clicked
 
         $I->expectTo('see cube info');
-        $I->see('Cube Settings:');
-        $I->see('N: 4');
+        $I->see('Settings:');
+        $I->see('Test 1 of 2');
     }
 }
--- tests/functional/IndexFormCest.php
+++ tests/functional/IndexFormCest.php
@@ -9,37 +9,27 @@ class IndexFormCest
 
     public function submitEmptyForm(\FunctionalTester $I)
     {
-        $I->submitForm('#cubeSetting-form', []);
+        $I->submitForm('#cubeTest-form', []);
         $I->expectTo('see validations errors');
         $I->see('T Tests cannot be blank');
-        $I->see('N Coordinates cannot be blank');
-        $I->see('M Operations cannot be blank');
     }
 
     public function submitFormWithIncorrectField(\FunctionalTester $I)
     {
-        $I->submitForm('#cubeSetting-form', [
-            'CubeSettingForm[T_tests]' => 'words',
-            'CubeSettingForm[N_coordinates]' => '5d3',
-            'CubeSettingForm[M_operations]' => '33.33',
+        $I->submitForm('#cubeTest-form', [
+            'CubeTestsForm[T_tests]' => 'words',
         ]);
-        $I->expectTo('see that all the fields are wrong');
+        $I->expectTo('see alert message of wrong field type');
         $I->see('T Tests must be an integer.');
-        $I->see('N Coordinates must be an integer.');
-        $I->see('M Operations must be an integer.');
     }
 
     public function submitFormSuccessfully(\FunctionalTester $I)
     {
-        $I->submitForm('#cubeSetting-form', [
-          'CubeSettingForm[T_tests]' => '2',
-          'CubeSettingForm[N_coordinates]' => '4',
-          'CubeSettingForm[M_operations]' => '5',
+        $I->submitForm('#cubeTest-form', [
+          'CubeTestsForm[T_tests]' => '2',
         ]);
-        $I->dontSeeElement('#cubeSetting-form');
-        $I->see('Cube Settings:');
-        $I->see('T: 2');
-        $I->see('N: 4');
-        $I->see('M: 5');
+        $I->dontSeeElement('#cubeTest-form');
+        $I->see('Settings');
+        $I->see('Test 1 of 2');
     }
 }
